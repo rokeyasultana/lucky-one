9014362d6b56e10694a55b239600e82d3c1fb18b
jakiasultana2022 <rokeyasbd@gmail.com>
2022-03-26 10:15:18 +0600

    header

--- lucky-one/src/App.js
+++ lucky-one/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Shop from './components/Shop/Shop';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <Shop></Shop>
+    <Header></Header>
     </div>
   );
 }
---
+++ lucky-one/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='Watches'>
+         <h1>World of Watches</h1>
+         <p>Buy your favourite one...</p>
+        </div>
+    );
+};
+
+export default Header;
